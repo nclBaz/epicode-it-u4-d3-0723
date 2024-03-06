b0ddb58215a4b837680bd62c6e04809632857e5a
Riccardo Gulin <riccardo.gulin@gmail.com>
2024-03-06 12:03:31 +0100

    Adds equals & toString examples

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import entities.Student;
-import es1.Esercizio1;
 
 public class Main {
 	public static void main(String[] args) {
@@ -7,20 +6,34 @@ public class Main {
 		Student aldo = new Student(); // I COSTRUTTORE
 		Student giovanni = new Student("Giovanni"); // II COSTRUTTORE
 		Student giacomo = new Student("Giacomo", "Poretti"); // III COSTRUTTORE
+		Student giacomo2 = new Student("Giacomo", "Poretti");
+		giacomo2.id = giacomo.id;
 
 		Student ajeje = aldo; // NON COPIARE MAI GLI OGGETTI COSI'
 		ajeje.name = "Ajeje";
 
-		aldo.sayHello();
+		// Esempi di utilizzo di metodi non statici
+/*		aldo.sayHello();
 		giovanni.sayHello();
 		giacomo.sayHello();
 		ajeje.sayHello();
 
+		// Esempi di utilizzo di metodi statici
 		System.out.println(Math.PI);
-
 		System.out.println(Esercizio1.isBisestile(2024));
+		System.out.println(Student.school);*/
 
-		System.out.println(Student.school);
+		System.out.println(aldo);
+		System.out.println(giovanni);
+		System.out.println(giacomo);
+		System.out.println(giacomo2);
+		System.out.println(ajeje);
+
+		if (giacomo.equals(giacomo2)) {
+			System.out.println("I due giacomo sono uguali");
+		} else {
+			System.out.println("Sono diversi");
+		}
 	}
 
 
--- src/entities/Student.java
+++ src/entities/Student.java
@@ -1,5 +1,6 @@
 package entities;
 
+import java.util.Objects;
 import java.util.Random;
 
 public class Student {
@@ -43,4 +44,30 @@ public class Student {
 		System.out.println("Ciao sono " + this.name + " " + this.surname + ", ID: " + this.id);
 	}
 
+
+	// To String e Equals sono metodi di default che tutti gli oggetti possiedono alla loro creazione
+	// Spessissimo però non mi vanno bene questi comportamenti
+	// Di default il toString mi stampa cose tipo entities.Student@1b28cdfa e allora lo voglio customizzare
+	// Di default l'equals mi ritorna true solo se 2 oggetti sono nella stessa cella di memoria, però
+	// non è detto che ciò mi vada bene, magari vorrei fare un controllo del tipo: se hanno gli stessi
+	// valori allora equals mi deve tornare true. E allora sovrascrivo anch'esso
+
+	// Per far generare questi 2 metodi a Intellij basta fare tasto dx --> Generate
+	@Override
+	public String toString() {
+		return "Student{" +
+				"id=" + id +
+				", name='" + name + '\'' +
+				", surname='" + surname + '\'' +
+				'}';
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true; // Se i 2 oggetti rappresentano la stessa cella di memoria allora torna true
+		if (o == null || getClass() != o.getClass())
+			return false; // Se o è null oppure se appartengono a classi diverse ritorna false
+		Student student = (Student) o; // casting esplicito
+		return this.id == student.id && Objects.equals(this.name, student.name) && Objects.equals(this.surname, student.surname);
+	}
 }
